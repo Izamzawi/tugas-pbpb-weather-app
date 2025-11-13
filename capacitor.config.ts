f2995a18a87e6658c1cbd54faa33585c0523e2e2
Idham <idhamzamzawi@gmail.com>
2025-11-13 07:29:03 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tugas2-weather-app',
+  webDir: 'dist'
+};
+
+export default config;
